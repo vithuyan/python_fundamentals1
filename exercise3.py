ceed417896b30dcb1a75c676aea6c609d5cd32fc
Vithuyan Sivananthan <vithuyan.s@outlook.com>
2019-02-11 18:06:13 -0500

    done

--- exercise3.py
+++ exercise3.py
@@ -1,1 +1,8 @@
 print("What is your name?")
+user_name = input()
+print("Hello, {}".format(user_name))
+
+print ("How old are you?")
+age = input()
+remaining_years = 2019 - int(age)
+print ("You are born in {} .". format (remaining_years))
