425e5ec4c426f5aab0327bced370f80ee4f5e697
Vithuyan Sivananthan <vithuyan.s@outlook.com>
2019-02-11 17:24:26 -0500

    done

---
+++ exercise2.py
@@ -0,0 +1,21 @@
+# Question 1
+print (55 * 0.10)
+
+# Question 2
+
+age = "20"
+print (age)
+
+int_age = int(age)
+
+print(int_age)
+
+print("")
+
+# Question 3
+
+print ("45628 times 7839 is {}".format(45628 * 7839))
+
+# Question 4
+
+print (10 < 20 and 30 < 20) or not(10 == 11)
